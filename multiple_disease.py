fc6c7b63db0fcc3f0330f1cab7ed21c2567964a0
Thibsuda <130614298+Thibsuda@users.noreply.github.com>
2024-02-28 11:15:18 -0800

    Update multiple_disease.py

--- multiple_disease.py
+++ multiple_disease.py
@@ -95,10 +95,47 @@ if (selected == 'Heart Disease Prediction'):
     st.success(heart_dignosis)
 
 # Infomation
-if selected == "Data dictionary":
+if selected == "Info":
 
     # page title
     st.title("Data dictionary")
- 
-    
+
+    # Info Box with Paragraph
+    st.info("""
+        This is an information show you the information that you need to put on prediction example gender male put 1,female put 0 into the box.
+        Sentence 1.age - age in years
+        Sentence 2.sex - (1 = male; 0 = female)
+        Sentence 3.cp - chest pain type
+    * 0: Typical angina: chest pain related decrease blood supply to the heart
+    * 1: Atypical angina: chest pain not related to heart
+    * 2: Non-anginal pain: typically esophageal spasms (non heart related)
+    * 3: Asymptomatic: chest pain not showing signs of disease        
+        Sentence 4.trestbps - resting blood pressure (in mm Hg on admission to the hospital) anything above 130-140 is typically cause for concern
+        Sentence 5.chol - serum cholestoral in mg/dl
+    * serum = LDL + HDL + .2 * triglycerides
+    * above 200 is cause for concern        
+        Sentence 6. fbs - (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+    * '>126' mg/dL signals diabetes        
+        Sentence 7.restecg - resting electrocardiographic results
+    * 0: Nothing to note
+    * 1: ST-T Wave abnormality
+        * can range from mild symptoms to severe problems
+        * signals non-normal heart beat
+    * 2: Possible or definite left ventricular hypertrophy
+        * Enlarged heart's main pumping chamber
+        Sentence 8.thalach - maximum heart rate achieved
+        Sentence 9.exang - exercise induced angina (1 = yes; 0 = no) do you get heart pain when you do exercise
+        Sentence 10. oldpeak - ST depression induced by exercise relative to rest looks at stress of heart during excercise unhealthy heart will stress more    """)
+        Sentence 11. slope - the slope of the peak exercise ST segment
+    * 0: Upsloping: better heart rate with excercise (uncommon)
+    * 1: Flatsloping: minimal change (typical healthy heart)
+    * 2: Downslopins: signs of unhealthy heart
+        Sentence 12.ca - number of major vessels (0-3) colored by flourosopy
+    * colored vessel means the doctor can see the blood passing through
+    * the more blood movement the better (no clots)
+    Sentence 13.thal - thalium stress result
+    * 1,3: normal
+    * 6: fixed defect: used to be defect but ok now
+    * 7: reversable defect: no proper blood movement when excercising
+    Sentence 14.target - have disease or not (1=yes, 0=no) (= the predicted attribute)    
     
